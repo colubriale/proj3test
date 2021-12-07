b9bc0212eb26dc941caf1ff85a24e034b84e65ec
colubriale <84979695+colubriale@users.noreply.github.com>
2021-12-08 01:14:36 +1100

    Add files via upload

--- proposals/dapp.js
+++ proposals/dapp.js
@@ -2,37 +2,6 @@
 const contract_address = "0x085D15A0965303C669f22368cDaC48903E304199"; 
 
 const dApp = {
-  setEncrypt: function(source,destination,passcode) {
-      document.getElementById(destination).innerText = this.encryptCodes(document.getElementById(source).value,document.getElementById(passcode).value);
-  },
-  setDecrypt: function() {
-              document.getElementById('decryptedContent').innerText = this.decryptCodes(document.getElementById('originalContent').value,document.getElementById('passcode').value);
-  },
-  encryptCodes: function(content,passcode) {
-      var result = []; var passLen = passcode.length ;
-      for(var i = 0  ; i < content.length ; i++) {
-          var passOffset = i%passLen ;
-          var calAscii = (content.charCodeAt(i)+passcode.charCodeAt(passOffset));
-          result.push(calAscii);
-      }
-      return JSON.stringify(result) ;
-  },
-  decryptCodes: function(content,passcode) {
-      var result = [];var str = '';
-      var codesArr = JSON.parse(content);var passLen = passcode.length ;
-      for(var i = 0  ; i < codesArr.length ; i++) {
-          var passOffset = i%passLen ;
-          var calAscii = (codesArr[i]-passcode.charCodeAt(passOffset));
-          result.push(calAscii) ;
-      }
-      for(var i = 0 ; i < result.length ; i++) {
-          var ch = String.fromCharCode(result[i]); str += ch ;
-      }
-      return str ;
-  }
-
-
-
   ethEnabled: function() {
     // If the browser has MetaMask installed
     if (window.ethereum) {
